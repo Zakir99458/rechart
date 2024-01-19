35637b8d022bf938207a0ad9ecd3b17bde864a81
Mohammad Zakir Hossain <engineer.zakir.hossain@gmail.com>
2024-01-20 00:08:39 +0200

    line-chart-added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import MyLineChart from './components/LineChart/MyLineChart';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <MyLineChart></MyLineChart>
     </div>
   );
 }
